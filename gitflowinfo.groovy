432e6b8af46fae36a9751a527e26f7d98e31bd8d
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 12:23:59 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -3,6 +3,6 @@ node {
     def branchApi = new URL("https://api.github.com/repos/${project}/branches")
     def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
     stage('Clone sources') {
-        git url: 'https://github.com/Netflix/asgard.git'
+        println("branches")
     }
 }
