876b6a94a7211c014e58d90874401dbbbe38b835
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 12:56:28 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,17 +1,57 @@
+AUTH = "логин и пароль в Base64"                           
+GIT_URL = "url до вашей SCM (https://bitbucket.org/)"                       
+PROJECT_NAME = "имя проектной области, где находятся репозитории"
 
-def project = 'EugeneKalashnikov/DockerBuilds'
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-branches.each {
-    def branchName = it.name
-    def jobName = "${project}-${branchName}".replaceAll('/','-')
-    println(branchName)
-}
-node{
-    stage('Clone sources') {
-        parameters{ text(name: 'mytextparam', 
-                 defaultValue: 'Default lines for the parameter', 
-                 description: 'A description of this param')    
-        }
-        git url: 'https://github.com/jfrogdev/project-examples.git'
+def htmlBuild() {
+    html = """
+            <html>
+            <head>
+            <meta charset="windows-1251">
+            <style type="text/css">
+            div.grayTable {
+            text-align: left;
+            border-collapse: collapse;
+            }
+            .divTable.grayTable .divTableCell, .divTable.grayTable .divTableHead {
+            padding: 0px 3px;
+            }
+            .divTable.grayTable .divTableBody .divTableCell {
+            font-size: 13px;
+            }
+            </style>
+            </head>
+            <body>
+        """
+
+    def commitOptions = ""
+    getCommitsForMicroservice(MICROSERVICE_NAME).each {
+        commitOptions += "<option style='font-style: italic' value='COMMIT=${it.getKey()}'>${it}</option>"
     }
+    html += """<p style="display: inline-block;">
+        <select id="commit_id" size="1" name="value">
+            ${commitOptions}
+        </select></p></div>"""
+
+    html += """
+            </div>
+            </div>
+            </div>
+            </body>
+            </html>
+         """
+    return html
+}
+
+def getCommitsForMicroservice(microserviceRepo) {
+    def commits = [:]
+    def endpoint = GIT_URL + "/rest/api/1.0/projects/${PROJECT_NAME}/repos/${microserviceRepo}/commits"
+    def conn = new URL(endpoint).openConnection()
+    conn.setRequestProperty("Authorization", "Basic ${AUTH}")
+    def response = new groovy.json.JsonSlurper().parseText(conn.content.text)
+    response.values.each {
+        commits.put(it.displayId, it.message)
+    }
+    return commits
+}
+
+return htmlBuild()
