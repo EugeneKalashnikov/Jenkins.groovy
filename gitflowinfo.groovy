b50554d67d633098d5f73da51808b908969a4864
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 20:59:42 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,15 +1,17 @@
-pipelineJob('browse-branches') {
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            url('https://github.com/EugeneKalashnikov/DockerBuilds.git')
+pipelineJob('testpipeline-build') {
+	description('Build test docker image, test and push it to local registry')
+	definition {
+    	cpsScm {
+      	scm {
+          git {
+            branch('origin/master')
+            remote {
+              url('git@gitlab-selfhosted.org:test-group/sample-project.git')
+              credentials('gitlab-creds')
+            }
           }
-          branch('*/master')
-        }
-      }
-      lightweight()
+      	}
+      	scriptPath('Jenkinsfile')
+    	}
     }
-  }
 }
