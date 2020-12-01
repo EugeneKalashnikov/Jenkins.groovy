3e13f53fdb711e8e5f50dbbe2677599b37e2b90e
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 20:35:53 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,38 +1,15 @@
-job('item1') {
-	description()
-	keepDependencies(false)
-	parameters {
-		activeChoiceParam("Branche") {
-			description()
-			groovyScript {
-				script("return [\"master\",\"dev\"]")
-				fallbackScript("")
-			}
-			choiceType("SINGLE_SELECT")
-			filterable(false)
-		}
-		stringParam("repourl", "https://github.com/EugeneKalashnikov/DockerBuilds.git", "")
-		stringParam("chechouthash", "", "")
-	}
-	definition {
-		cpsScm {
-"""println "\${params.repourl}"
-println "\${params.chechouthash}"
-node {
-    sh label: '1', script: "git checkout \${params.Branche}"
-    sh label: '2', script: 'git log'
-    sh label: '3', script: "git checkout \${params.chechouthash}"
-}"""		}
-	}
-	disabled(false)
-	configure {
-		it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
-			strategy {
-				'daysToKeep'('5')
-				'numToKeep'('5')
-				'artifactDaysToKeep'('-1')
-				'artifactNumToKeep'('-1')
-			}
-		}
-	}
+pipelineJob('browse-branches') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/EugeneKalashnikov/DockerBuilds.git')
+          }
+          branch('*/master')
+        }
+      }
+      lightweight()
+    }
+  }
 }
