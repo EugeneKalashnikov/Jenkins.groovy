36d8e2cd00a242fb42432b336e24268894ad62a3
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 12:32:13 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,26 +1,19 @@
-for (aSlave in hudson.model.Hudson.instance.slaves) {
-  println('====================');
-  println('Name: ' + aSlave.name);
-  println('getLabelString: ' + aSlave.getLabelString());
-  println('getNumExectutors: ' + aSlave.getNumExecutors());
-  println('getRemoteFS: ' + aSlave.getRemoteFS());
-  println('getMode: ' + aSlave.getMode());
-  println('getRootPath: ' + aSlave.getRootPath());
-  println('getDescriptor: ' + aSlave.getDescriptor());
-  println('getComputer: ' + aSlave.getComputer());
-  println('\tcomputer.isAcceptingTasks: ' + aSlave.getComputer().isAcceptingTasks());
-  println('\tcomputer.isLaunchSupported: ' + aSlave.getComputer().isLaunchSupported());
-  println('\tcomputer.getConnectTime: ' + aSlave.getComputer().getConnectTime());
-  println('\tcomputer.getDemandStartMilliseconds: ' + aSlave.getComputer().getDemandStartMilliseconds());
-  println('\tcomputer.isOffline: ' + aSlave.getComputer().isOffline());
-  println('\tcomputer.countBusy: ' + aSlave.getComputer().countBusy());
-  //if (aSlave.name == 'NAME OF NODE TO DELETE') {
-  //  println('Shutting down node!!!!');
-  //  aSlave.getComputer().setTemporarilyOffline(true,null);
-  //  aSlave.getComputer().doDoDelete();
-  //}
-  println('\tcomputer.getLog: ' + aSlave.getComputer().getLog());
-  println('\tcomputer.getBuilds: ' + aSlave.getComputer().getBuilds());
+import hudson.model.*
+ 
+for(item in Hudson.instance.items) {
+  prop = item.getProperty(ParametersDefinitionProperty.class)
+  if(prop != null) {
+    println("--- Parameters for " + item.name + " ---")
+    for(param in prop.getParameterDefinitions()) {
+      try {
+        println(param.name + " " + param.defaultValue)
+      }
+      catch(Exception e) {
+        println(param.name)
+      }
+    }
+    println()
+  }
 }
 node {
     //def project = 'Netflix/asgard'
