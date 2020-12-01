cae3307f2795ff6c59e8465f67e595a61653a828
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 22:13:23 +0300

    Create another.groovy

---
+++ another.groovy
@@ -0,0 +1,35 @@
+def repourl = 'https://github.com/EugeneKalashnikov/DockerBuilds.git'
+properties([
+    parameters([
+        gitParameter(branch: '',
+                     branchFilter: 'origin/(.*)',
+                     defaultValue: 'master',
+                     description: '',
+                     name: 'BRANCH',
+                     quickFilterEnabled: false,
+                     selectedValue: 'NONE',
+                     sortMode: 'NONE',
+                     tagFilter: '*',
+                     useRepository: 'https://github.com/EugeneKalashnikov/DockerBuilds.git',
+                     type: 'PT_BRANCH'),
+        string(name: 'COMMIT')
+    ])
+])
+
+pipeline {
+    agent any
+    stages {
+        stage('Initial') {
+            steps {
+                checkout([$class: 'GitSCM', 
+                          branches: [[name: "${params.TAG}"]], 
+                          doGenerateSubmoduleConfigurations: false, 
+                          extensions: [], 
+                          gitTool: 'Default', 
+                          submoduleCfg: [], 
+                          userRemoteConfigs: [[url: "${repourl}"]]]
+                        ])
+            }
+        }
+    }
+}
