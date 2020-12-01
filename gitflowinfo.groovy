f48e84f54a61a0fb67cf0bcc2f2d1ab22e748adb
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 15:56:26 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -13,7 +13,12 @@ properties([
           classpath: [], 
           sandbox: false, 
           script: 
-            'return[\"Development\",\"Production\"]'
+          '''
+          def gettags = ("git ls-remote -t -h https://github.com/EugeneKalashnikov/DockerBuilds.git").execute()
+          return gettags.text.readLines().collect { 
+             it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '').replaceAll("\\^\\{\\}", '')
+          }
+          '''
         ]
       ]
     ],
