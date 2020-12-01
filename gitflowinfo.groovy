f41434a2dd14e2ce547b6a5de8d8ede9e340198e
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 13:04:07 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,2 +1,18 @@
-choice(name: 'ENV', choices: product, description: 'Env'),
-choice(name: 'ENV_NO', choices: envParams(product), description: 'Env No')
+Reactive Parameter “projectBranchName”):
+
+AUTH = "loginAndPassInBase64"
+GIT_URL = "https://github.com"
+PROJECT = "EugeneKalashnikov/DockerBuilds"
+ 
+def getBranchesFromRepoName(repoName) {
+    def branches = []
+    def endpoint = GIT_URL + "/rest/api/1.0/projects/${PROJECT}/repos/${repoName}/branches"
+    def conn = new URL(endpoint).openConnection()
+    //conn.setRequestProperty("Authorization", "Basic ${AUTH}")
+    def response = new groovy.json.JsonSlurper().parseText(conn.content.text)
+    response.values.each {
+        branches.add(it.displayId)
+    }
+    return branches
+}
+return getBranchesFromRepoName(repoName)
