396a34a2e0a97adf463cc93de5d3991c3afeeb70
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 21:22:18 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,15 +1,78 @@
-pipelineJob('job-dsl-plugin') {
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            url('https://github.com/jenkinsci/job-dsl-plugin.git')
-          }
-          branch('*/master')
-        }
-      }
-      lightweight()
-    }
-  }
+def repourl = 'https://github.com/EugeneKalashnikov/DockerBuilds.git'
+properties([
+  parameters([
+    [$class: 'CascadeChoiceParameter', 
+      choiceType: 'PT_SINGLE_SELECT', 
+      description: 'Select environment',
+      filterLength: 1,
+      filterable: false,
+      name: 'Environment', 
+      script: [
+        $class: 'GroovyScript', 
+        script: [
+          classpath: [], 
+          sandbox: false, 
+          script: 
+            'return[\"Development\",\"Production\"]'
+        ]
+      ]
+    ],
+    [$class: 'CascadeChoiceParameter', 
+      choiceType: 'PT_SINGLE_SELECT', 
+      description: 'Select Site',
+      filterLength: 1,
+      filterable: false,
+      referencedParameters: 'Environment',
+      name: 'Site', 
+      script: [
+        $class: 'GroovyScript', 
+        script: [
+          classpath: [], 
+          sandbox: false, 
+          script: 
+            '''if (Environment.equals("Development")){
+    def command = $/ git log --pretty=oneline /$
+        def proc = command.execute()
+        return proc.text.readLines()
 }
+else if(Environment.equals("Production")){
+    def command = $/ git log --pretty=oneline /$
+        def proc = command.execute()
+        return proc.text.readLines()
+}'''
+        ]
+      ]
+    ],
+    [$class: 'CascadeChoiceParameter', 
+        choiceType: 'PT_SINGLE_SELECT', 
+        description: 'Select Folder. \'.\' - Remove all site folder',
+        filterLength: 1,
+        filterable: false,
+        referencedParameters: 'Environment, Site',
+        name: 'Folder', 
+        script: [
+          $class: 'GroovyScript', 
+          script: [
+            classpath: [], 
+            sandbox: false, 
+            script: 
+              '''if (Environment.equals("Development")){
+    def command = $/ git log --pretty=oneline /$
+        def proc = command.execute()
+        return proc.text.readLines()
+}
+else if(Environment.equals("Production")){
+    def command = $/ git log --pretty=oneline/$
+        def proc = command.execute()
+        return proc.text.readLines()
+}'''
+        ]
+      ]
+    ],
+    [$class: 'BooleanParameterDefinition', 
+      description: 'Set this to confirm deletion',
+      name: 'Confirm',
+      defaultValue: false
+    ]
+  ])
+])
