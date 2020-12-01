db407f064609a7295c6130ee110db7ceb1de44d8
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 15:12:14 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,30 +1,78 @@
 def repourl = 'https://github.com/EugeneKalashnikov/DockerBuilds.git'
 properties([
-    parameters([
-        gitParameter(branch: '',
-                     branchFilter: 'origin/(.*)',
-                     defaultValue: 'master',
-                     description: '',
-                     name: 'BRANCH',
-                     quickFilterEnabled: false,
-                     selectedValue: 'NONE',
-                     sortMode: 'NONE',
-                     tagFilter: '*',
-                     useRepository: repourl,
-                     type: 'PT_BRANCH'),
-         gitParameter(branch1: '',
-                     branchFilter: 'origin/(.*)',
-                     defaultValue: 'master',
-                     description: '',
-                     name: 'BRANCH',
-                     quickFilterEnabled: false,
-                     selectedValue: 'NONE',
-                     sortMode: 'NONE',
-                     tagFilter: '*',
-                     useRepository: repourl,
-                     type: 'PT_BRANCH')       
-    ])
-])
-node {
-    git branch: "${params.BRANCH}", url: repourl
+  parameters([
+    [$class: 'CascadeChoiceParameter', 
+      choiceType: 'PT_SINGLE_SELECT', 
+      description: 'Select environment',
+      filterLength: 1,
+      filterable: false,
+      name: 'Environment', 
+      script: [
+        $class: 'GroovyScript', 
+        script: [
+          classpath: [], 
+          sandbox: false, 
+          script: 
+            'return[\'Development\',\'Production\']'
+        ]
+      ]
+    ],
+    [$class: 'CascadeChoiceParameter', 
+      choiceType: 'PT_SINGLE_SELECT', 
+      description: 'Select Site',
+      filterLength: 1,
+      filterable: false,
+      referencedParameters: 'Environment',
+      name: 'Site', 
+      script: [
+        $class: 'GroovyScript', 
+        script: [
+          classpath: [], 
+          sandbox: false, 
+          script: 
+            '''if (Environment.equals("Development")){
+    def command = $/ssh centos@192.168.1.101 sudo ls /var/www/html /$
+        def proc = command.execute()
+        return proc.text.readLines()
 }
+else if(Environment.equals("Production")){
+    def command = $/ssh centos@192.168.1.102 sudo ls /var/www/html /$
+        def proc = command.execute()
+        return proc.text.readLines()
+}'''
+        ]
+      ]
+    ],
+    [$class: 'CascadeChoiceParameter', 
+        choiceType: 'PT_SINGLE_SELECT', 
+        description: 'Select Folder. \'.\' - Remove all site folder',
+        filterLength: 1,
+        filterable: false,
+        referencedParameters: 'Environment, Site',
+        name: 'Folder', 
+        script: [
+          $class: 'GroovyScript', 
+          script: [
+            classpath: [], 
+            sandbox: false, 
+            script: 
+              '''if (Environment.equals("Development")){
+    def command = $/ssh centos@192.168.1.101 sudo ls -a /var/www/html'/'${Site} | grep -v '\\.\\.' /$
+        def proc = command.execute()
+        return proc.text.readLines()
+}
+else if(Environment.equals("Production")){
+    def command = $/ssh centos@192.168.1.102 sudo ls -a /var/www/html'/'${Site} | grep -v '\\.\\.'/$
+        def proc = command.execute()
+        return proc.text.readLines()
+}'''
+        ]
+      ]
+    ],
+    [$class: 'BooleanParameterDefinition', 
+      description: 'Set this to confirm deletion',
+      name: 'Confirm',
+      defaultValue: false
+    ]
+  ])
+])
