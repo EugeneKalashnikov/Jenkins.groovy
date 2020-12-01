91f7d9ee6e94a1d686c61e4d1dc97d861fa1c111
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 11:55:25 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,9 +1,4 @@
 node {
-    // Get Artifactory server instance, defined in the Artifactory Plugin administration page.
-    def server = Artifactory.server "SERVER_ID"
-    // Create an Artifactory Maven instance.
-    def rtMaven = Artifactory.newMavenBuild()
-    def buildInfo
 
     stage('Clone sources') {
         git url: 'https://github.com/jfrogdev/project-examples.git'
