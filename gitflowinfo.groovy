4c9a02a947d01da374b457ce4d08f64b7bd1e778
Евгений Калашников <37065696+EugeneKalashnikov@users.noreply.github.com>
2020-12-01 12:40:38 +0300

    Update gitflowinfo.groovy

--- gitflowinfo.groovy
+++ gitflowinfo.groovy
@@ -1,25 +1,9 @@
-import hudson.model.*
- 
-for(item in Hudson.instance.items) {
-  prop = item.getProperty(ParametersDefinitionProperty.class)
-  if(prop != null) {
-    println("--- Parameters for " + item.name + " ---")
-    for(param in prop.getParameterDefinitions()) {
-      try {
-        println(param.name + " " + param.defaultValue)
-      }
-      catch(Exception e) {
-        println(param.name)
-      }
-    }
-    println()
-  }
-}
-node {
-    //def project = 'Netflix/asgard'
-    //def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-    //def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-    stage('Clone sources') {
-        println("branches")
-    }
+def project = 'quidryan/aws-sdk-test'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    println(branchName)
+
 }
